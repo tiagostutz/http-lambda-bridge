b09fa17491020290e0ddad32ecdf60d5b77cb68c
Tiago Stutz <tiagostutz@gmail.com>
2021-06-19 17:53:44 -0300

    demo: add docker-compose to show that the image can be ran both in a Docker cluster or in a lambda environment

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Easily convert any HTTP service into a Lambda application
 ## Parameters
 
 - LOG_LEVEL:
-- INIT_TIMEOUT:
-- 
+- PROXY_PASS:
+- HTTP_SERVICE_INIT_TIMEOUT:
--- demo/startup.sh
+++ demo/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# start the lambda bridge
+# start the lambda bridge in background
 echo 'Starting /bin/http-lambda-bridge in background'
 nohup /bin/http-lambda-bridge --logLevel="$LOG_LEVEL" --proxyPass="$PROXY_PASS" --httpServiceInitTimeout=$HTTP_SERVICE_INIT_TIMEOUT  & # start the http service
 
 echo 'Starting HTTP service as the main process'
+
 ##############################################################
 ## change the above lines with your service startup command ##
 ##############################################################
-echo 'starting json-server'
+echo 'starting json-server in foreground'
 json-server --watch /data/demo-data.json
